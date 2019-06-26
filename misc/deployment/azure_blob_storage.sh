60fea92fec9e35b305d6b373195967190b8964a3
Boon Wenjie <boonwenjie@gmail.com>
2019-06-26 16:36:28 +0100

    STORAGE: Add setup script for Azure Blob Storage
    Uses Azure CLI to perform the following steps
    - Creates resource group
    - Creates storage account
    - Creates containers

---
+++ misc/deployment/azure_blob_storage.sh
@@ -0,0 +1,24 @@
+RES_GRP="holorepository-rg"
+LOC="uksouth"
+BLOB_STORE_NAME="holoblob"
+
+az login
+
+az group create \
+    --name ${RES_GRP} \
+    --location ${LOC} 
+
+az storage account create \
+    --name ${BLOB_STORE_NAME} \
+    --resource-group ${RES_GRP} \
+    --location ${LOC} \
+    --sku Standard_LRS \
+    --kind StorageV2
+
+KEY=$(az storage account keys list \
+    --account-name ${BLOB_STORE_NAME} \
+    --resource-group ${RES_GRP} \
+    --output table | grep "key1" | awk '{print $3}')
+
+az storage container create --account-key ${KEY} --account-name ${BLOB_STORE_NAME} --name "3dmodels"
+az storage container create --account-key ${KEY} --account-name ${BLOB_STORE_NAME} --name "attachments"

d62c5d4a40d9c9869b1197392c6db8d4c3891e97
Niels Boecker <niels.boecker@gmail.com>
2019-07-01 17:21:26 +0100

    Add test dependencies and write test for routing capabilities

---
+++ HoloRepositoryUI/src/components/core/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import MainContainer from "./MainContainer";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MainContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
--- HoloRepositoryUI/src/index.tsx
+++ HoloRepositoryUI/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/shared/App";
+import App from "./components/core/App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));

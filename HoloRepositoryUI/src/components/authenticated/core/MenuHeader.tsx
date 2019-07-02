f442e45c496c8ba83acc0dc5e0fee88e884921a3
Niels Boecker <niels.boecker@gmail.com>
2019-07-02 16:01:47 +0100

    UI: Implement Menu

--- HoloRepositoryUI/src/components/authenticated/core/MenuHeader.tsx
+++ HoloRepositoryUI/src/components/authenticated/core/MenuHeader.tsx
@@ -5,8 +5,11 @@ import { Image, IImageProps, ImageFit } from "office-ui-fabric-react/lib/Image";
 import "antd/dist/antd.css";
 import "./MenuHeader.scss";
 import appLogo from "../../../assets/logo/2x/logo_and_font@2x.png";
+import { Link, navigate } from "@reach/router";
+import { Practitioner } from "../../../types";
 
 const { Header } = Layout;
+const { SubMenu } = Menu;
 
 const imageProps: IImageProps = {
   src: appLogo,
@@ -18,9 +21,11 @@ class MenuHeader extends Component {
   render() {
     return (
       <Header>
-        <div className="logo">
-          <Image {...(imageProps as any)} alt="HoloRepository logo" />
-        </div>
+        <Link to="/app/patients">
+          <div className="logo">
+            <Image {...(imageProps as any)} alt="HoloRepository logo" />
+          </div>
+        </Link>
 
         <Menu
           theme="light"
@@ -28,12 +33,45 @@ class MenuHeader extends Component {
           defaultSelectedKeys={["patients"]}
           style={{ lineHeight: "64px" }}
         >
-          <Menu.Item key="patients">Patients</Menu.Item>
-          <Menu.Item key="holograms">Holograms</Menu.Item>
-          <Menu.Item key="devices">Devices</Menu.Item>
+          <Menu.Item key="patients" onClick={({ key }) => this.doNavigate(key)}>
+            Patients
+          </Menu.Item>
+          <Menu.Item
+            key="holograms"
+            onClick={({ key }) => this.doNavigate(key)}
+          >
+            Holograms
+          </Menu.Item>
+          <Menu.Item key="devices" onClick={({ key }) => this.doNavigate(key)}>
+            Devices
+          </Menu.Item>
+
+          <SubMenu
+            title={<span className="submenu-title-wrapper">User Name</span>}
+            style={{ float: "right" }}
+          >
+            <Menu.Item
+              key="profile"
+              onClick={({ key }) => this.doNavigate(key)}
+            >
+              Show profile
+            </Menu.Item>
+            <Menu.Item key="logout" onClick={() => this.doLogout()}>
+              Log out
+            </Menu.Item>
+          </SubMenu>
         </Menu>
       </Header>
     );
+  }
+
+  doLogout(): void {
+    console.log("Log out");
+    navigate("/");
+  }
+
+  doNavigate(component: string) {
+    navigate(`/app/${component}`);
   }
 }
 

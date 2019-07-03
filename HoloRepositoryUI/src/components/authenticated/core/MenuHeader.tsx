9d8573fabd247fc6d20aa01a18e4f02b41e27669
Niels Boecker <niels.boecker@gmail.com>
2019-07-03 20:47:49 +0100

    UI: Add user persona to menu header

--- HoloRepositoryUI/src/components/authenticated/core/MenuHeader.tsx
+++ HoloRepositoryUI/src/components/authenticated/core/MenuHeader.tsx
@@ -5,11 +5,19 @@ import {
   IImageProps,
   ImageFit
 } from "office-ui-fabric-react/lib-commonjs/Image";
+import {
+  IPersonaSharedProps,
+  Persona,
+  PersonaSize,
+  PersonaPresence
+} from "office-ui-fabric-react/lib-commonjs/Persona";
 
 import "antd/dist/antd.css";
 import "./MenuHeader.scss";
-import appLogo from "../../../assets/logo/2x/logo_and_font@2x.png";
 import { Link, navigate } from "@reach/router";
+import appLogo from "../../../assets/logo/2x/logo_and_font@2x.png";
+import samplePractitioner from "../../../__tests__/samples/samplePractitioner.json";
+import { IPractitioner } from "../../../types";
 
 const { Header } = Layout;
 const { SubMenu } = Menu;
@@ -18,6 +26,17 @@ const imageProps: IImageProps = {
   src: appLogo,
   imageFit: ImageFit.cover,
   maximizeFrame: true
+};
+
+const practitioner = samplePractitioner as IPractitioner;
+
+const practitionerPersona: IPersonaSharedProps = {
+  imageUrl: practitioner.pictureUrl,
+  imageInitials: "NS",
+  text: practitioner.name.full,
+  secondaryText: "Practitioner"
+  //tertiaryText: 'In a meeting',
+  //optionalText: 'Available at 4:00pm'
 };
 
 class MenuHeader extends Component {
@@ -34,7 +53,7 @@ class MenuHeader extends Component {
           theme="light"
           mode="horizontal"
           defaultSelectedKeys={["patients"]}
-          style={{ lineHeight: "66px" }}
+          style={{ lineHeight: "64px" }}
         >
           <Menu.Item key="patients" onClick={({ key }) => this.doNavigate(key)}>
             Patients
@@ -50,8 +69,17 @@ class MenuHeader extends Component {
           </Menu.Item>
 
           <SubMenu
-            title={<span className="submenu-title-wrapper">User Name</span>}
-            style={{ float: "right" }}
+            title={
+              <div className="submenu-title-wrapper">
+                <Persona
+                  {...practitionerPersona}
+                  size={PersonaSize.size32}
+                  presence={PersonaPresence.online}
+                  hidePersonaDetails={false}
+                />
+              </div>
+            }
+            style={{ float: "right", padding: "16px 0" }}
           >
             <Menu.Item
               key="profile"

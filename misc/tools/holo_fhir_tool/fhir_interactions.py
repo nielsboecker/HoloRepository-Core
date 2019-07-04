4d2bd3e64387a6b5dadf48573cc6e40adb00af11
Wenjie <boonwenjie@gmail.com>
2019-07-04 02:35:05 +0100

    STORAGE: Add delete-all fhir data functionality
    delete-all queries for all fhir resources in the server and requests
    deletion for every resource found.

--- misc/tools/holo_fhir_tool/fhir_interactions.py
+++ misc/tools/holo_fhir_tool/fhir_interactions.py
@@ -12,16 +12,25 @@ class FHIRInteraction(object):
         self._base_url = base_url
         self._headers = {'Content-Type': 'application/fhir+json'}
     
-    def _upload(self, url: str, content: dict, action: str):
+    def _request(self, url: str, action: str, body: dict = {}):
         if action == 'PUT':
-            r = requests.put(url, headers=self._headers, data=json.dumps(content))
+            r = requests.put(url, headers=self._headers, data=json.dumps(body))
         elif action == 'POST':
-            r = requests.post(url, headers=headers, data=json.dumps(content))
+            r = requests.post(url, headers=headers, data=json.dumps(body))
+        elif action == 'DELETE':
+            r = requests.delete(url)
+        elif action == 'GET':
+            r = requests.get(url)
+
         print(f'{r.status_code} - {action} to {url}')
-        if r.status_code == 400:
+
+        if not r.ok:
             sys.exit(r.json())
 
-    def bundle_file(self, fhir_json: str):
+        return r
+
+
+    def upload_bundle(self, fhir_json: str):
         content = None
         with open(fhir_json, 'r') as fhir_f:
             content = json.load(fhir_f)
@@ -33,10 +42,25 @@ class FHIRInteraction(object):
             if entry['fullUrl'].index('urn:uuid:') == 0:
                 id = entry['fullUrl'].split('urn:uuid:')[1]
                 url = urljoin(self._base_url, '/'.join([entry['request']['url'], id]))
-                self._upload(url, entry['resource'], 'PUT') 
+                self._request(url, 'PUT', entry['resource']) 
             else:
                 url = urljoin(self._base_url, entry['request']['url'])
-                self._upload(url, entry['resource'], 'POST') 
+                self._request(url, 'POST', entry['resource']) 
+
+
+    def delete_all(self):
+        next_url = self._base_url
+        while next_url:
+            r = self._request(next_url, 'GET')
+            data = r.json()
+            next_url = None
+            
+            for link in data['link']:
+                if link['relation'] == "next":
+                    next_url = link['url'] 
+            
+            for entry in data.get('entry', []):
+                self._request(entry['fullUrl'], 'DELETE')
 
 
 if __name__ == "__main__":

4034cca2a32f248427995c4308c761f78a56b0f5
Wenjie <boonwenjie@gmail.com>
2019-07-04 12:24:26 +0100

    STORAGE: Refac az_blob_tool
    - Hide service property from being exposed in cli
    - Line formattings for code > 79 char

--- misc/tools/storage-tester/az_blob_tool.py
+++ misc/tools/storage-tester/az_blob_tool.py
@@ -19,17 +19,17 @@ class Holoblob:
         else:
             sys.exit('Config file not found')
 
-        self.service = BlockBlobService(account_name=configs['account_name'],
+        self._service = BlockBlobService(account_name=configs['account_name'],
                                         account_key=configs['account_key'])
 
-    def create_container(self, container_name, public_access=False):
+    def create_container(self, container_name, public=False):
         """Creates containers within storage account"""
         try:
             access = None
-            if public_access:
+            if public:
                 access = PublicAccess.Container
 
-            self.service.create_container(container_name, public_access=access)
+            self._service.create_container(container_name, public_access=access)
             print(f"Container {container_name} to be created")
 
         except Exception as e:
@@ -40,9 +40,11 @@ class Holoblob:
         try:
             name, ext = os.path.splitext(os.path.basename(file_path))
             local_file_name = name + "-" + str(uuid.uuid4()) + ext
-            self.service.create_blob_from_path(container_name, local_file_name, file_path)
+            self._service.create_blob_from_path(container_name,
+                                               local_file_name,
+                                               file_path)
             print(f"{file_path} uploaded to {container_name} as {local_file_name}")
-            print(f"Access-URL: {self.service.make_blob_url(container_name, local_file_name)}")
+            print(f"Access-URL: {self._service.make_blob_url(container_name, local_file_name)}")
 
         except Exception as e:
             print(e)
@@ -50,7 +52,7 @@ class Holoblob:
     def list_blobs(self, container_name):
         """List all files within a container"""
         try:
-            generator = self.service.list_blobs(container_name)
+            generator = self._service.list_blobs(container_name)
             for blob in generator:
                 print("\t Blob name: " + blob.name)
 
@@ -60,7 +62,7 @@ class Holoblob:
     def list_containers(self):
         """List all containers in the blob storage"""
         try:
-            containers = self.service.list_containers()
+            containers = self._service.list_containers()
             for container in containers:
                 print("\t Container name: " + container.name)
         except Exception as e:
@@ -69,19 +71,19 @@ class Holoblob:
     def delete_blob(self, container_name, filename):
         """Delete a blob within a container"""
         try:
-            self.service.delete_blob(container_name, filename)
+            self._service.delete_blob(container_name, filename)
             print(f"Successful: {filename} deleted")
 
         except Exception as e:
             print(e)
 
     def delete_container(self, container_name):
-        """Triggers Azure to start the deletion of the container from storage.
+        """Triggers Azure to start the deletion of the container from storage
 
         This operation may not be instant
         """
         try:
-            self.service.delete_container(container_name)
+            self._service.delete_container(container_name)
             print(f"Successful: {container_name} sent for deletion")
 
         except Exception as e:

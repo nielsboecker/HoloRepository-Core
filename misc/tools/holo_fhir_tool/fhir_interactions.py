eddd53d3542534718ff223b3874ce29f122a08e3
Wenjie <boonwenjie@gmail.com>
2019-07-04 01:47:41 +0100

    STORAGE: Use fire library as cli tool
    Simplify cli interface using fire library.
    
    Note: Initialisation help message is not accurate, __ini__ fields can
    only be set via --flags. Not sure if this is a bug in the library.

--- misc/tools/holo_fhir_tool/fhir_interactions.py
+++ misc/tools/holo_fhir_tool/fhir_interactions.py
@@ -3,38 +3,41 @@
 import requests
 import json
 import sys
+import fire
 
 from urllib.parse import urljoin
 
-def upload(url: str, content: dict, action: str):
-    headers = {'Content-Type': 'application/fhir+json'}
-    if action == 'PUT':
-        r = requests.put(url, headers=headers, data=json.dumps(content))
-    elif action == 'POST':
-        r = requests.post(url, headers=headers, data=json.dumps(content))
-    print(f'{r.status_code} - {action} to {url}')
-    if r.status_code == 400:
-        sys.exit(r.json())
+class FHIRInteraction(object):
+    def __init__(self, base_url):
+        self._base_url = base_url
+        self._headers = {'Content-Type': 'application/fhir+json'}
+    
+    def _upload(self, url: str, content: dict, action: str):
+        if action == 'PUT':
+            r = requests.put(url, headers=self._headers, data=json.dumps(content))
+        elif action == 'POST':
+            r = requests.post(url, headers=headers, data=json.dumps(content))
+        print(f'{r.status_code} - {action} to {url}')
+        if r.status_code == 400:
+            sys.exit(r.json())
 
-def process_fhir_transaction(fhir_json: str, base_url: str):
-    content = None
-    with open(fhir_json, 'r') as fhir_f:
-        content = json.load(fhir_f)
+    def bundle_file(self, fhir_json: str):
+        content = None
+        with open(fhir_json, 'r') as fhir_f:
+            content = json.load(fhir_f)
 
-    if not (content['resourceType'] == "Bundle" and content['type'] == 'transaction'):
-        sys.exit('Selected file is not a FHIR transaction')
+        if not (content['resourceType'] == "Bundle" and content['type'] == 'transaction'):
+            sys.exit('Selected file is not a FHIR transaction')
 
-    for entry in content['entry']:
-        if entry['fullUrl'].index('urn:uuid:') == 0:
-            id = entry['fullUrl'].split('urn:uuid:')[1]
-            url = urljoin(base_url, '/'.join([entry['request']['url'], id]))
-            upload(url, entry['resource'], 'PUT') 
-        else:
-            url = urljoin(base_url, entry['request']['url'])
-            upload(url, entry['resource'], 'POST') 
+        for entry in content['entry']:
+            if entry['fullUrl'].index('urn:uuid:') == 0:
+                id = entry['fullUrl'].split('urn:uuid:')[1]
+                url = urljoin(self._base_url, '/'.join([entry['request']['url'], id]))
+                self._upload(url, entry['resource'], 'PUT') 
+            else:
+                url = urljoin(self._base_url, entry['request']['url'])
+                self._upload(url, entry['resource'], 'POST') 
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        sys.exit(f"To use: {sys.argv[0]} fhirTransactionJSON fhir_server_uri")
-    process_fhir_transaction(sys.argv[1], sys.argv[2])
+    fire.Fire(FHIRInteraction)

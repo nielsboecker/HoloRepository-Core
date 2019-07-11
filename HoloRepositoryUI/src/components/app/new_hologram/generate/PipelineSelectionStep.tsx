821a7099b17792cbb253b8159cf417c3d47e8751
Niels Boecker <niels.boecker@gmail.com>
2019-07-11 01:30:49 +0100

    UI: Add simple pipeline selection step (WIP)

--- HoloRepositoryUI/src/components/app/new_hologram/generate/PipelineSelectionStep.tsx
+++ HoloRepositoryUI/src/components/app/new_hologram/generate/PipelineSelectionStep.tsx
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
 
+import samplePipelines from "../../../../__tests__/samples/samplePipelines.json";
+import { IPipeline } from "../../../../types";
+import { ChoiceGroup, IChoiceGroupOption } from "office-ui-fabric-react";
+import ExtendedChoiceGroupLabel from "../shared/ExtendedChoiceGroupLabel";
+
+const pipelines = samplePipelines as IPipeline[];
+
+const choiceGroupOptions: IChoiceGroupOption[] = pipelines.map(pipeline => ({
+  key: pipeline.id,
+  text: pipeline.name,
+  onRenderLabel: () => (
+    <ExtendedChoiceGroupLabel
+      title={pipeline.name}
+      description={pipeline.description}
+      iconName="Blur"
+    />
+  )
+}));
+
 class PipelineSelectionStep extends Component {
   render() {
-    return <div>PipelineSelectionStep</div>;
+    return (
+      <div>
+        <ChoiceGroup options={choiceGroupOptions} label="Select a pipeline" />
+      </div>
+    );
   }
 }
 

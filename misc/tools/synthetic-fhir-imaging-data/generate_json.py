20d7b52fc572dda417419a8299440408152abfbc
Boon Wenjie <boonwenjie@gmail.com>
2019-07-22 18:06:13 +0100

    MISC: Add output functionality

--- misc/tools/synthetic-fhir-imaging-data/generate_json.py
+++ misc/tools/synthetic-fhir-imaging-data/generate_json.py
@@ -52,19 +52,24 @@ def generate_instances(instances, display):
 
 
 def modify_imaging_study(data, num_instances, display_name, url):
+    resource_data = data["resource"]
     num_instances = int(num_instances)
     tag = "imagingEndpointId"
-    data["numberOfSeries"] = 1
-    data["numberOfInstances"] = num_instances
-    data["endpoint"] = create_endpoint_reference(tag)
-    data["contained"] = create_contained_endpoint(tag, url)
-    data["subject"]["reference"] = data["subject"]["reference"].replace(
-        "urn:uuid:", "Patient/"
+    resource_data["numberOfSeries"] = 1
+    resource_data["numberOfInstances"] = num_instances
+    resource_data["endpoint"] = create_endpoint_reference(tag)
+    resource_data["contained"] = create_contained_endpoint(tag, url)
+    resource_data["subject"]["reference"] = resource_data["subject"][
+        "reference"
+    ].replace("urn:uuid:", "Patient/")
+    resource_data.pop("encounter", None)
+    resource_data["series"][0]["numberOfInstances"] = num_instances
+    resource_data["series"][0]["bodySite"]["display"] = display_name
+    resource_data["series"][0]["instance"] = generate_instances(
+        num_instances, display_name
     )
-    data.pop("encounter", None)
-    data["series"][0]["numberOfInstances"] = num_instances
-    data["series"][0]["bodySite"]["display"] = display_name
-    data["series"][0]["instance"] = generate_instances(num_instances, display_name)
+
+    data["resource"] = resource_data
 
     return data
 
@@ -77,28 +82,37 @@ def find_imaging_study(data):
         raise LookupError("ImagingStudy resource not found")
 
 
+def write_data(output_path, content):
+    dir_name = os.path.dirname(output_path)
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    with open(output_path, "w") as output_f:
+        output_f.write(json.dumps(content, indent=4, sort_keys=True))
+
+
 def main():
     for patient_file, info in config.CONFIG.items():
         title, num_instances, display_name, url = info
+        data = None
+        output = "output/" + os.path.basename(patient_file) + ".modified"
+        print(output)
         if not os.path.isfile(patient_file):
             sys.exit("{0} cannot be found".format(patient_file))
         with open(patient_file, "r") as read_patient_file:
             data = json.load(read_patient_file)
-            imaging_study_index = find_imaging_study(data)
-            new_imaging_study = modify_imaging_study(
-                data["entry"][imaging_study_index]["resource"],
-                num_instances,
-                display_name,
-                url,
-            )
-            modified_data = data
-            modified_data["entry"] = [
-                data["entry"][0],
-                new_imaging_study,
-            ]  # first entry is patients
 
-            print(json.dumps(modified_data))
-        # print(name, instances, display, url)
+        imaging_study_index = find_imaging_study(data)
+        new_imaging_study = modify_imaging_study(
+            data["entry"][imaging_study_index], num_instances, display_name, url
+        )
+        modified_data = data
+        modified_data["entry"] = [
+            data["entry"][0],
+            new_imaging_study,
+        ]  # first entry is patients
+
+        write_data(output, modified_data)
+    # print(name, instances, display, url)
 
 
 if __name__ == "__main__":

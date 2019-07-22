02414b1f55edfb62fba8538a4f7262d91af04d80
Boon Wenjie <boonwenjie@gmail.com>
2019-07-22 14:47:11 +0100

    MISC: Initial commit for Dummy FHIR Imaging Data
    Created configuratoins and README

---
+++ misc/tools/synthetic-fhir-imaging-data/README.md
@@ -0,0 +1,2 @@
+# Synthetic FHIR Imaging Data
+Tool used will generate dummy fhir data that points to hosted imaging studies endpoints
---
+++ misc/tools/synthetic-fhir-imaging-data/config.py
@@ -0,0 +1,42 @@
+CONFIG = {
+    "left-renal-mass": [
+        "862",
+        "pelvis",
+        "https://holoblob.blob.core.windows.net/mock-packs/left-renal-mass.zip",
+    ],
+    "left-scfe-pelvis-bone": [
+        "207",
+        "pelvis",
+        "https://holoblob.blob.core.windows.net/mock-packs/left-scfe-pelvis-bone.zip",
+    ],
+    "left-scfe-pelvis-soft": [
+        "207",
+        "pelvis",
+        "https://holoblob.blob.core.windows.net/mock-packs/left-scfe-pelvis-soft.zip",
+    ],
+    "normal-abdomen": [
+        "327",
+        "abdomen",
+        "https://holoblob.blob.core.windows.net/mock-packs/normal-abdomen.zip",
+    ],
+    "normal-chest-lung": [
+        "273",
+        "chest",
+        "https://holoblob.blob.core.windows.net/mock-packs/normal-chest-lung.zip",
+    ],
+    "normal-chest-mediastinal": [
+        "273",
+        "chest",
+        "https://holoblob.blob.core.windows.net/mock-packs/normal-chest-mediastinal.zip",
+    ],
+    "normal-pelvis-bone": [
+        "132",
+        "pelvis",
+        "https://holoblob.blob.core.windows.net/mock-packs/normal-pelvis-bone.zip",
+    ],
+    "normal-pelvis-soft": [
+        "132",
+        "pelvis",
+        "https://holoblob.blob.core.windows.net/mock-packs/normal-pelvis-soft.zip",
+    ],
+}

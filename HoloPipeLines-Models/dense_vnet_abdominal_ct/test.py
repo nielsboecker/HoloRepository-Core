f4f48b015ba9c951da61ada5ad6ed72dfee9ff32
Kawai <603110911@qq.com>
2019-07-23 16:44:54 +0100

    PIPELINES: Edit according to the new comments 23/7

--- HoloPipeLines-Models/dense_vnet_abdominal_ct/test.py
+++ HoloPipeLines-Models/dense_vnet_abdominal_ct/test.py
@@ -2,18 +2,20 @@ from server import app
 
 import unittest
 
+
 class FlaskTestCase(unittest.TestCase):
     def test_the_post_request_without_file_is_not_allowed(self):
-        tester=app.test_client(self)
-        response = tester.post('/model')
+        tester = app.test_client(self)
+        response = tester.post("/model")
         self.assertEqual(response.status_code, 400)
 
-    def test_the_post_request_with_file_can_be_successed(self):
-        tester=app.test_client(self)
-        f= open('c_CT.nii', 'rb') 
-        response = tester.post('/model',data={'file': f})
+    def test_the_post_request_with_file_can_be_succeed(self):
+        tester = app.test_client(self)
+        f = open("100_CT.nii", "rb")
+        response = tester.post("/model", data={"file": f})
         f.close()
         self.assertEqual(response.status_code, 200)
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     unittest.main()

d3bcd3cbb0f2a8971d9284267336b09e3773462c
UdomkarnBoonyaprasert <udomkarnb@gmail.com>
2019-07-25 17:03:07 +0100

    PIPELINE: ran code through black

--- HoloPipelines/pipelines/pipelineNifti2glb.py
+++ HoloPipelines/pipelines/pipelineNifti2glb.py
@@ -4,11 +4,22 @@ from components import compObj2glbWrapper
 import pathlib
 import sys
 
-def main(inputNiftiPath, outputGlbPath, threshold):
-	generatedNumpyList = compNifti2numpy.main(str(pathlib.Path(inputNiftiPath)))
-	generatedObjPath = compNumpy2obj.main(generatedNumpyList, threshold, str(pathlib.Path.cwd().joinpath("output", "OBJ", "nifti2glb_tempObj.obj")))
-	generatedGlbPath = compObj2glbWrapper.main(generatedObjPath, str(pathlib.Path(outputGlbPath)), deleteOriginalObj=True, compressGlb=False)
-	print("nifti2glb: done")
 
-if __name__ == '__main__':
-	main(sys.argv[1], sys.argv[2], sys.argv[3])
+def main(inputNiftiPath, outputGlbPath, threshold):
+    generatedNumpyList = compNifti2numpy.main(str(pathlib.Path(inputNiftiPath)))
+    generatedObjPath = compNumpy2obj.main(
+        generatedNumpyList,
+        threshold,
+        str(pathlib.Path.cwd().joinpath("output", "OBJ", "nifti2glb_tempObj.obj")),
+    )
+    generatedGlbPath = compObj2glbWrapper.main(
+        generatedObjPath,
+        str(pathlib.Path(outputGlbPath)),
+        deleteOriginalObj=True,
+        compressGlb=False,
+    )
+    print("nifti2glb: done")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
--- HoloPipelines/test/test_pipelineController.py
+++ HoloPipelines/test/test_pipelineController.py
@@ -5,30 +5,88 @@ import subprocess
 
 newCwd = str(pathlib.Path(str(os.path.dirname(os.path.realpath(__file__)))).parent)
 
+
 def test_main_s0_normal():
-	output = subprocess.run(["python", "pipelineController.py", "-c", "test/testList.json", "s0"], cwd=newCwd)
-	assert 0 == output.returncode
+    output = subprocess.run(
+        ["python", "pipelineController.py", "-c", "test/testList.json", "s0"],
+        cwd=newCwd,
+    )
+    assert 0 == output.returncode
+
 
 def test_main_s0_invalid_number_of_argument():
-	output = subprocess.run(["python", "pipelineController.py", "-c", "test/testList.json", "s0", "-p", "1"], cwd=newCwd)
-	assert 1 == output.returncode
+    output = subprocess.run(
+        [
+            "python",
+            "pipelineController.py",
+            "-c",
+            "test/testList.json",
+            "s0",
+            "-p",
+            "1",
+        ],
+        cwd=newCwd,
+    )
+    assert 1 == output.returncode
+
 
 def test_main_missing_pipeline():
-	output = subprocess.run(["python", "pipelineController.py", "-c", "test/testList.json", "s3"], cwd=newCwd)
-	assert 1 == output.returncode
+    output = subprocess.run(
+        ["python", "pipelineController.py", "-c", "test/testList.json", "s3"],
+        cwd=newCwd,
+    )
+    assert 1 == output.returncode
+
 
 def test_main_s0_missing_pipeline_with_extra_arguments():
-	output = subprocess.run(["python", "pipelineController.py", "-c", "test/testList.json", "s3", "-p", "0"], cwd=newCwd)
-	assert 1 == output.returncode
+    output = subprocess.run(
+        [
+            "python",
+            "pipelineController.py",
+            "-c",
+            "test/testList.json",
+            "s3",
+            "-p",
+            "0",
+        ],
+        cwd=newCwd,
+    )
+    assert 1 == output.returncode
+
 
 def test_main_ls_normal():
-	output = subprocess.run(["python", "pipelineController.py", "-c", "test/testList.json", "--ls"], cwd=newCwd)
-	assert 0 == output.returncode
+    output = subprocess.run(
+        ["python", "pipelineController.py", "-c", "test/testList.json", "--ls"],
+        cwd=newCwd,
+    )
+    assert 0 == output.returncode
+
 
 def test_main_info_dicom2glb():
-	output = subprocess.run(["python", "pipelineController.py", "-c", "test/testList.json", "--info", "dicom2glb"], cwd=newCwd)
-	assert 0 == output.returncode
+    output = subprocess.run(
+        [
+            "python",
+            "pipelineController.py",
+            "-c",
+            "test/testList.json",
+            "--info",
+            "dicom2glb",
+        ],
+        cwd=newCwd,
+    )
+    assert 0 == output.returncode
+
 
 def test_main_info_for_missing_pipeline():
-	output = subprocess.run(["python", "pipelineController.py", "-c", "test/testList.json", "--info", "doesNotExist"], cwd=newCwd)
-	assert 1 == output.returncode
+    output = subprocess.run(
+        [
+            "python",
+            "pipelineController.py",
+            "-c",
+            "test/testList.json",
+            "--info",
+            "doesNotExist",
+        ],
+        cwd=newCwd,
+    )
+    assert 1 == output.returncode

7a97ee5bc0238fdf2f7d4fc4935782c3e9112fa9
Kawai <603110911@qq.com>
2019-07-26 20:11:10 +0100

    PIPELINES: add the set up for the test

--- HoloPipeLines-Models/dense_vnet_abdominal_ct/test.py
+++ HoloPipeLines-Models/dense_vnet_abdominal_ct/test.py
@@ -1,9 +1,28 @@
 from server import app
 
 import unittest
+import tarfile
+import requests
 
 
 class FlaskTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        url = 'https://www.dropbox.com/s/5fk0m9v12if5da9/dense_vnet_abdominal_ct_model_zoo_data.tar.gz?dl=1'
+        r = requests.get(url, allow_redirects=True)
+        open('dense_vnet_abdominal_ct_model_zoo_data.tar.gz', 'wb').write(r.content)
+        fname="dense_vnet_abdominal_ct_model_zoo_data.tar.gz"
+        if (fname.endswith("tar.gz")):
+            tar = tarfile.open(fname, "r:gz")
+            tar.extractall()
+            tar.close()
+        elif (fname.endswith("tar")):
+            tar = tarfile.open(fname, "r:")
+            tar.extractall()
+            tar.close()
+
+
+
     def test_the_post_request_without_file_is_not_allowed(self):
         tester = app.test_client(self)
         response = tester.post("/model")

dd02ab4599dfcabaf284fc1956ca46c08fdbb2f2
Niels Boecker <niels.boecker@gmail.com>
2019-07-27 17:29:01 +0100

    UI: Prepare functionality to download and delete holograms

--- HoloRepositoryUI-Client/src/services/holoRepositoryServerService.ts
+++ HoloRepositoryUI-Client/src/services/holoRepositoryServerService.ts
@@ -7,6 +7,7 @@ import {
   IPipeline
 } from "../../../HoloRepositoryUI-Types";
 import { PidToPatientsMap } from "../components/shared/AppState";
+import { AxiosResponse } from "axios";
 
 const handleError = (error: Error): null => {
   console.log("Encountered an error while fetching data from backend: ", error.message);
@@ -48,17 +49,26 @@ export class HoloRepositoryServerService {
       .catch(handleError);
   }
 
-  public async downloadHologramById(hid: string): Promise<BinaryType | null> {
+  public async downloadHologramById(hid: string): Promise<boolean | null> {
     return serverAxios
-      .get<BinaryType>(`${routes.hologram}/${hid}/download`)
-      .then(hologram => hologram.data)
+      .get<BinaryType>(
+        // Note: Mocked for now; change to `${routes.hologram}/${hid}/download`
+        "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Duck/glTF-Binary/Duck.glb",
+        {
+          responseType: "blob",
+          headers: {
+            Accept: "gltf-binary"
+          }
+        }
+      )
+      .then(file => _forceDownload(file, `${hid}.glb`))
       .catch(handleError);
   }
 
   public async deleteHologramById(hid: string): Promise<boolean | null> {
     return serverAxios
       .delete(`${routes.hologram}/${hid}`)
-      .then(() => true)
+      .then(response => response.status === 200 || response.status === 204)
       .catch(handleError);
   }
 
@@ -102,6 +112,17 @@ export class HoloRepositoryServerService {
 
 const _extractCombinedPidsString = (patients: PidToPatientsMap): string => {
   return Object.keys(patients).join(",");
+};
+
+// Note: see https://gist.github.com/javilobo8/097c30a233786be52070986d8cdb1743
+const _forceDownload = (response: AxiosResponse, fileName: string = "hologram.glb") => {
+  const url = window.URL.createObjectURL(new Blob([response.data]));
+  const link = document.createElement("a");
+  link.href = url;
+  link.setAttribute("download", fileName);
+  document.body.appendChild(link);
+  link.click();
+  return true;
 };
 
 export default new HoloRepositoryServerService();

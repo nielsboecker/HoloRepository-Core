1f6208ad68c7c5f0f15b5b44674d1300035e794e
UdomkarnBoonyaprasert <udomkarnb@gmail.com>
2019-08-06 11:12:31 +0100

    PIPELINE: add dicom2glb sitk -add dicom2glb sitk -add comp to generate multiple numpy from one (exclusive for abdomen NN model)

--- HoloPipelines/pipelines/components/compDcm2nifti.py
+++ HoloPipelines/pipelines/components/compDcm2nifti.py
@@ -1,5 +1,7 @@
 import dicom2nifti
 import os
+import glob
+import pathlib
 
 
 def main(dicomInputPath, outputNiftiFolderPath):
@@ -9,7 +11,9 @@ def main(dicomInputPath, outputNiftiFolderPath):
     # convert series of dicom to nifti
     dicom2nifti.convert_directory(dicomInputPath, outputNiftiFolderPath)
     print("dcm2nifti: done")
-    return outputNiftiFolderPath
+    lsdir = glob.glob(str(pathlib.Path(outputNiftiFolderPath).joinpath("*.nii.gz")))
+    outputNiftiPath = str(pathlib.Path(lsdir[0]))
+    return outputNiftiPath
 
 
 if __name__ == "__main__":

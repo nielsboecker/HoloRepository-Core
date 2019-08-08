7d561d762a91d1cb82725e8bfc9049ba8a7d2da5
Niels Boecker <niels.boecker@gmail.com>
2019-08-08 14:00:23 +0100

    UI/BE: Change redirection status code to 308

--- HoloRepositoryUI/server/src/api/routes/holograms/holograms.controller.ts
+++ HoloRepositoryUI/server/src/api/routes/holograms/holograms.controller.ts
@@ -3,27 +3,27 @@ import { Request, Response } from "express";
 
 export class HologramsController {
   public async getAll(req: Request, res: Response): Promise<void> {
-    res.redirect(HologramsService.getAllForPatientsURL(req.query.pids));
+    res.redirect(308, HologramsService.getAllForPatientsURL(req.query.pids));
   }
 
   public downloadById(req: Request, res: Response): void {
-    res.redirect(HologramsService.getDownloadURL(req.params.hid));
+    res.redirect(308, HologramsService.getDownloadURL(req.params.hid));
   }
 
   public deleteById(req: Request, res: Response): void {
-    res.redirect(307, HologramsService.getEndpointURL(req.params.hid));
+    res.redirect(308, HologramsService.getEndpointURL(req.params.hid));
   }
 
   public upload(req: Request, res: Response): void {
     // Note: Ideally, the request should be checked for validity
     // Note: Upload models goes directly to the POST /holograms of HoloStorageAccessor
-    res.redirect(307, HologramsService.getBaseURL());
+    res.redirect(308, HologramsService.getBaseURL());
   }
 
   public generate(req: Request, res: Response): void {
     // Note: Ideally, the request should be checked for validity
     // Note: Upload models goes directly to the POST /job of HoloPipelines
-    res.redirect(307, HologramsService.getPipelineJobURL());
+    res.redirect(308, HologramsService.getPipelineJobURL());
   }
 }
 

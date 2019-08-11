4bf2602fa06dd0195df1fdc5312de5f31afc772f
Niels Boecker <niels.boecker@gmail.com>
2019-08-11 14:36:31 +0100

    UI: Add axios-mock-adapter to mock network calls

--- HoloRepositoryUI/client/src/setupTests.ts
+++ HoloRepositoryUI/client/src/setupTests.ts
@@ -4,8 +4,15 @@
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { initializeIcons } from "@uifabric/icons";
+import holoRepositoryServerAxios from "./services/holoRepositoryServerAxios";
+import MockAdapter from "axios-mock-adapter";
 
+// Configure Enzyme adaptedr
 configure({ adapter: new Adapter() });
+
+// Configure Axios mock adapter to prevent all actual network calls and just return 200 for now
+const mock = new MockAdapter(holoRepositoryServerAxios);
+mock.onAny().reply(200);
 
 // Note: See https://developer.microsoft.com/en-us/fabric/#/styles/web/icons#fabric-react
 initializeIcons();

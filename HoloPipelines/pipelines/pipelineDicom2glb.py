9f444676646558bebd51a24ee6f9a651256cf6bb
localadmin <udomkarnb@gmail.com>
2019-08-15 18:20:52 +0100

    PIPELINE: add cleanup component

--- HoloPipelines/pipelines/pipelineDicom2glb.py
+++ HoloPipelines/pipelines/pipelineDicom2glb.py
@@ -5,13 +5,17 @@ from pipelines.components import compNumpy2obj
 from pipelines.components import compObj2glbWrapper
 from pipelines.components import compPostToAccesor
 from pipelines.components import compCombineInfoForAccesor
+from pipelines.components import compFetchResource
+from pipelines.components import compJobCleanup
 from datetime import datetime
 import pathlib
 import json
 import sys
 
 
-def main(jobID, dicomFolderPath, outputGlbPath, infoForAccessor):
+def main(jobID, dicomDownloadUrl, infoForAccessor):
+    compJobStatus.update_status(jobID, "Fetching data")
+    dicomFolderPath = compFetchResource.main(jobID, dicomDownloadUrl)
     compJobStatus.update_status(jobID, "Pre-processing")
     generatedNumpyList = compDicom2numpy.main(str(pathlib.Path(dicomFolderPath)))
     threshold = 300
@@ -29,23 +33,28 @@ def main(jobID, dicomFolderPath, outputGlbPath, infoForAccessor):
     )
     compJobStatus.update_status(jobID, "3D format conversion")
     generatedGlbPath = compObj2glbWrapper.main(
-        generatedObjPath, outputGlbPath, deleteOriginalObj=True, compressGlb=False
+        generatedObjPath,
+        str(pathlib.Path(dicomFolderPath).parent.joinpath(str(jobID) + ".glb")),
+        deleteOriginalObj=True,
+        compressGlb=False,
     )
     print("dicom2glb: done, glb saved to {}".format(generatedGlbPath))
-    compJobStatus.update_status(jobID, "Finished")
+    compJobStatus.update_status(jobID, "Posting data")
 
     infoForAccessor = compCombineInfoForAccesor.add_info_for_accesor(
         infoForAccessor,
         "apply on generic bone segmentation",
         datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
         "Generate glb mesh from dicom",
-        outputGlbPath,
+        generatedGlbPath,
     )
     print(json.dumps(infoForAccessor))
     print(datetime.now())
     compPostToAccesor.sendFilePostRequestToAccessor(infoForAccessor)
+    compJobStatus.update_status(jobID, "Finished")
     print(datetime.now())
+    compJobCleanup.main(jobID)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    main(sys.argv[1], sys.argv[2], sys.argv[3])

52e42cc02cc8d78e10397ce7e2ca1beba46100c6
Niels Boecker <niels.boecker@gmail.com>
2019-08-17 15:38:13 +0100

    PIPELINE: Delete unused compGetInput.py

--- HoloPipelines/pipelines/tasks/receive_input.py
+++ HoloPipelines/pipelines/tasks/receive_input.py
@@ -5,6 +5,8 @@ import pathlib
 import logging
 import requests
 from zipfile import ZipFile
+from pathlib import Path
+from flask_json import json_response
 
 this_comp_path = str(pathlib.Path(str(os.path.dirname(os.path.realpath(__file__)))))
 
@@ -38,3 +40,26 @@ def fetch_and_unzip(jobID, image_url):
             os.mkdir(file_path)
     download_content = get_content_from_url(image_url)
     return unzip_the_file(download_content, str(pathlib.Path(file_list[-1])))
+
+
+# TODO: This was an unused method in compGetInput.py (which itself was unused).
+#  Sort out if this does anything different from the above method
+input_directory = Path("input")
+def fetch_request_input_file(filename, request_input_data_URL):
+    this_cwd = Path.cwd()
+    response = requests.get(request_input_data_URL)
+    if response.status_code != 200:
+        return json_response(message="image study endpoint failure", status_code=404)
+    file_dir = this_cwd.joinpath(str(input_directory), filename)
+    open(file_dir, "wb+").write(response.content)
+    logging.debug("file dir: " + str(file_dir))
+
+    filename_unzip = filename.rsplit(".", 1)[0]
+    logging.debug("unzip file name:" + filename_unzip)
+
+    # unzip the input file
+    file_dir = this_cwd.joinpath(str(input_directory), filename)
+    path_to_store_unzip_file = this_cwd.joinpath(str(input_directory), filename_unzip)
+    with ZipFile(io.BytesIO(response.content), "r") as zipObj:
+        zipObj.extractall(str(path_to_store_unzip_file))
+    return str(path_to_store_unzip_file)

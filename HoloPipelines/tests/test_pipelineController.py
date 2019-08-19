b8ab4c35b5c050ddd6464d67bf5e7cf29ede1c25
Niels Boecker <niels.boecker@gmail.com>
2019-08-19 16:25:34 +0100

    PIPELINE: Remove unnecessary files

--- HoloPipelines/tests/test_pipelineController.py
+++ HoloPipelines/tests/test_pipelineController.py
@@ -6,30 +6,14 @@ import subprocess
 newCwd = str(pathlib.Path(str(os.path.dirname(os.path.realpath(__file__)))).parent)
 
 
-def test_main_samplePipeline_normal():
-    output = subprocess.run(
-        [
-            "python",
-            "pipeline_runner.py",
-            "-c",
-            "tests/pipelineListForTesting.json",
-            "samplePipeline",
-            "-p",
-            "1",
-        ],
-        cwd=newCwd,
-    )
-    assert 1 == output.returncode
-
-
 def test_main_missing_pipeline():
     output = subprocess.run(
         [
             "python",
             "pipeline_runner.py",
             "-c",
-            "tests/pipelineListForTesting.json",
-            "samplePipeline0",
+            "./core/pipelines/pipelines_list.json",
+            "ThisPipelineIdDoesNotExist",
         ],
         cwd=newCwd,
     )
@@ -42,8 +26,8 @@ def test_main_samplePipeline_missing_pipeline_with_extra_arguments():
             "python",
             "pipeline_runner.py",
             "-c",
-            "tests/pipelineListForTesting.json",
-            "samplePipeline0",
+            "./core/pipelines/pipelines_list.json",
+            "ThisPipelineIdDoesNotExist",
             "-p",
             "0",
         ],
@@ -58,7 +42,7 @@ def test_main_ls_normal():
             "python",
             "pipeline_runner.py",
             "-c",
-            "tests/pipelineListForTesting.json",
+            "./core/pipelines/pipelines_list.json",
             "--ls",
         ],
         cwd=newCwd,
@@ -72,7 +56,7 @@ def test_main_info_dicom2glb():
             "python",
             "pipeline_runner.py",
             "-c",
-            "tests/pipelineListForTesting.json",
+            "./core/pipelines/pipelines_list.json",
             "--info",
             "DICOM to glb",
         ],
@@ -87,7 +71,7 @@ def test_main_info_for_missing_pipeline():
             "python",
             "pipeline_runner.py",
             "-c",
-            "tests/pipelineListForTesting.json",
+            "./core/pipelines/pipelines_list.json",
             "--info",
             "doesNotExist",
         ],

1ec46247ed72da76ef1b156dee1e393e25bdf9a9
Niels Boecker <niels.boecker@gmail.com>
2019-08-20 19:02:23 +0100

    PIPELINE: Implement POST to HoloStorageAccessor

--- HoloPipelines/core/tasks/shared/dispatch_output.py
+++ HoloPipelines/core/tasks/shared/dispatch_output.py
@@ -1,5 +1,5 @@
 from core.clients.holostorage_accessor import send_file_request_to_accessor
 
 
-def dispatch_output(meta_data):
-    send_file_request_to_accessor(meta_data)
+def dispatch_output(job_id: str, plid: str, medical_data: dict):
+    send_file_request_to_accessor(job_id, plid, medical_data)

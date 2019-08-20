0667a49d376387898d91e865c46d8f4d982827ff
Niels Boecker <niels.boecker@gmail.com>
2019-08-20 05:02:02 +0100

    PIPELINE: Fix bug in obj2gltf wrapper

--- HoloPipelines/core/wrappers/obj2gltf.py
+++ HoloPipelines/core/wrappers/obj2gltf.py
@@ -4,19 +4,21 @@ import pathlib
 import subprocess
 import sys
 
-
 new_cwd = str(pathlib.Path(str(os.path.dirname(os.path.realpath(__file__)))))
 
 success = True
 
 
-def call_obj2gltf(obj_input_path, glb_output_path):
-    # TODO: Paths vs strings
-    obj_input_path = str(pathlib.Path(obj_input_path))
-    glb_output_path = str(pathlib.Path(glb_output_path))
-    obj2gltf_command = (
-        f"obj2gltf --binary --input {obj_input_path} --output {glb_output_path}"
-    )
+def call_obj2gltf(obj_input_path: str, glb_output_path: str):
+    obj2gltf_command = [
+        "obj2gltf",
+        "--binary",
+        "--input",
+        obj_input_path,
+        "--output",
+        glb_output_path,
+    ]
+
     completed_process = subprocess.run(obj2gltf_command)
     if completed_process.returncode == 0:
         logging.info("obj2glb: conversion complete")

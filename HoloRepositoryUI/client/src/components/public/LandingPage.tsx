de72c983c4804f9d7e4239ab06d4110607f2c6e5
Fanbo Meng <46536567+fanbomeng97@users.noreply.github.com>
2019-08-20 11:08:57 +0100

    UI: Updated login page

--- HoloRepositoryUI/client/src/components/public/LandingPage.tsx
+++ HoloRepositoryUI/client/src/components/public/LandingPage.tsx
@@ -1,13 +1,88 @@
 import React, { Component } from "react";
 import { Link, RouteComponentProps } from "@reach/router";
+import {
+  Dropdown,
+  IDropdownOption,
+  TextField,
+  PrimaryButton,
+  IImageProps,
+  ImageFit,
+  Image
+} from "office-ui-fabric-react";
+import { Layout } from "antd";
+import appLogo from "../../assets/logo/2x/logo_and_font@2x.png";
+import MainFooter from "../shared/MainFooter";
+
+const options: IDropdownOption[] = [
+  { key: "b0016666-1924-455d-8b16-92c631fa5207", text: "Isaac Upton" },
+  { key: "a100", text: "Maudie Kirlin" },
+  { key: "a101", text: "Erlinda Franecki" },
+  { key: "a102", text: "Jonah Schroeder" }
+];
+
+const imageProps: IImageProps = {
+  src: appLogo,
+  imageFit: ImageFit.cover,
+  maximizeFrame: true
+};
 
 class LandingPage extends Component<RouteComponentProps> {
+  backgroundStyle = {
+    background: "rgba(1,1,1,0.2)"
+  };
+
+  loginLogo = {
+    width: "420px",
+    height: "70px",
+    background: "rgba(255, 255, 255, 0.2)",
+    margin: "14px 14px 14px 14px"
+  };
+
+  LoginContainer = {
+    backgroundColor: "white",
+    maxHeight: "325px",
+    minWidth: "500px",
+    maxWidth: "850px",
+    padding: "24px"
+  };
+
   render() {
     return (
-      <div>
-        <h1>HoloRepository</h1>
-        <Link to="/app/patients">Login</Link>
-      </div>
+      <Layout style={{ ...this.backgroundStyle, height: "100vh" }}>
+        <Layout.Content
+          style={{
+            padding: "0 50px",
+            marginTop: "200px",
+            display: "flex",
+            justifyContent: "center"
+          }}
+        >
+          <div style={{ ...this.LoginContainer }}>
+            <div style={{ ...this.loginLogo }}>
+              <Image {...(imageProps as any)} alt="app_logo" />
+            </div>
+            <Dropdown
+              placeholder="Select an practitioner"
+              label="Practitioner:"
+              options={options}
+            />
+            <TextField label="Password:" type="password" style={{ width: "300" }} />
+            <div
+              style={{
+                marginTop: "20px",
+                display: "flex",
+                justifyContent: "center"
+              }}
+            >
+              <Link to="/app/patients">
+                <PrimaryButton data-automation-id="login" text="Login" allowDisabledFocus={true} />
+              </Link>
+            </div>
+          </div>
+        </Layout.Content>
+
+        <MainFooter style={this.backgroundStyle} />
+      </Layout>
     );
   }
 }

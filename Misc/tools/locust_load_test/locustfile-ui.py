276cdca5649ba166bf71bbe9b8fb2bb464b4407d
Boon Wenjie <boonwenjie@gmail.com>
2019-08-21 13:55:43 +0100

    MISC: Add locust load test for UI component

---
+++ Misc/tools/locust_load_test/locustfile-ui.py
@@ -0,0 +1,45 @@
+from locust import HttpLocust, TaskSet, task
+
+
+class UserBehavior(TaskSet):
+    @task
+    def getPatients(self):
+        self.client.get("/api/v1/patients")
+
+    @task
+    def getMultiplePatients(self):
+        self.client.get("/api/v1/patients?pids=p100,p102")
+
+    @task
+    def getSpecificPatient(self):
+        self.client.get("/api/v1/patients/p100")
+
+    @task
+    def getPractitioner(self):
+        self.client.get("/api/v1/practitioners/a100")
+
+    @task
+    def getImagingStudies(self):
+        self.client.get("/api/v1/imagingStudies")
+
+    @task
+    def getImagingStudiesOfPatients(self):
+        self.client.get("/api/v1/imagingStudies/pids=p100,p102")
+
+    @task
+    def getSpecificImagingStudies(self):
+        self.client.get("/api/v1/imagingStudies/i100")
+
+    @task
+    def getHolograms(self):
+        self.client.get("/api/v1/holograms")
+
+    @task
+    def getMultipleHolograms(self):
+        self.client.get("/api/v1/holograms?hids=h100,h102")
+
+
+class AccessorLocust(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 2000
+    max_wait = 5000

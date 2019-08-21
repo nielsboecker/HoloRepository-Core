2c7e2999da420327f13950fbb80c08b89d88693a
Niels Boecker <niels.boecker@gmail.com>
2019-08-21 01:33:24 +0100

    PIPELINE: Refactor and improve split_to_separate_organs task

--- HoloPipelines/core/tasks/abdominal_organs_segmentation/split_to_separate_organs.py
+++ HoloPipelines/core/tasks/abdominal_organs_segmentation/split_to_separate_organs.py
@@ -2,9 +2,11 @@
 
 # TODO: Depending on how we deciide for this pipeline, this component may get removed.
 # Otherwise, separate organs need to be merged again!
+# TODO: If we keep this, refactor and verify
+# Refactoring should include only doing the OBJs in here, and transforming to GLB on
+# pipeline level
 
 import logging
-import pathlib
 
 import numpy as np
 
@@ -13,34 +15,47 @@ from core.adapters.obj_file import write_mesh_as_obj
 from core.services.marching_cubes import generate_mesh
 
 
-def split_to_separate_organs(originalNumpy, outputPath):
-    outputPath = pathlib.Path(outputPath)
+def get_organ_name(hu_value: int):
+    """
+    Maps the "HU valuee" to the according organ name. Note that this is just an
+    assumption. The paper (Automatic multi-organ segmentation on abdominal CT with
+    dense v-networks https://doi.org/10.1109/TMI.2018.2806309) doesn't actually state
+    the mapping. But it always uses this order, thus the assumption. Also, I manually
+    compared outputs with Google image search :-) Note that 0 is how the authors
+    encoded empty space around the organs.
+    """
+    organs = [
+        None,
+        "Spleen",
+        "Left_Kidney",
+        "Gallbladder",
+        "Esophagus",
+        "Liver",
+        "Stomach",
+        "Pancreas",
+        "Duodenum",
+    ]
+    return organs[hu_value]
 
-    # get frequency
-    unique, counts = np.unique(originalNumpy, return_counts=True)
 
-    logging.info(np.asarray(unique))
-    logging.info(np.asarray((unique, counts)).T)
+def split_to_separate_organs(input: np.ndarray, output_directory_path: str):
+    logging.info("Separating different organs")
+    # Each segmented organ has been masked with a unique "HU value"
+    unique_hu_values = np.unique(input)
 
-    outputGlbPathList = []
+    # Value 0 is the empty space, remove
+    unique_hu_values = unique_hu_values[1:]
 
-    for integer in unique:
-        if integer != 0:
-            singleHUnumpy = originalNumpy == integer
-            singleHUnumpy = singleHUnumpy.astype(int)
-            threshold = 0
+    for hu_value in unique_hu_values:
+        organ_name = get_organ_name(hu_value)
+        logging.info(f"Processing organ: {organ_name} [{hu_value}]")
+        organ_mask = input == hu_value
+        organ_mask = organ_mask.astype(int)
+        threshold = 0
 
-            obj_output_path = pathlib.Path.cwd().joinpath(
-                "temp" + str(integer) + ".obj"
-            )
-            verts, faces, norm = generate_mesh(singleHUnumpy, threshold)
-            write_mesh_as_obj(verts, faces, norm, obj_output_path)
+        obj_output_path = f"{output_directory_path}/Organ_{hu_value}.obj"
+        glb_output_path = f"{output_directory_path}/Organ_{hu_value}.glb"
+        verts, faces, norm = generate_mesh(organ_mask, threshold)
+        write_mesh_as_obj(verts, faces, norm, obj_output_path)
 
-            convert_obj_to_glb_and_write(
-                pathlib.Path.cwd().joinpath("temp" + str(integer) + ".obj"),
-                outputPath.joinpath("organNo" + str(integer) + ".glb"),
-            )
-            outputGlbPathList.append(
-                str(outputPath.joinpath("organNo" + str(integer) + ".glb"))
-            )
-    return outputGlbPathList
+        convert_obj_to_glb_and_write(obj_output_path, glb_output_path)

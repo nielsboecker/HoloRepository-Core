184efee8db3e5fa72a641c9a15f196c206ba0634
Kawai Wong <603110911@qq.com>
2019-08-23 11:04:20 +0100

    Add conversion from numpy to obj and obj to glb

--- HoloPipelines/core/pipelines/abdominal_organs_segmentation.py
+++ HoloPipelines/core/pipelines/abdominal_organs_segmentation.py
@@ -10,20 +10,29 @@ multi-organ segmentation on abdominal CT with dense v-networks https://doi.org/1
 """
 
 import sys
+import os
 
 from config import MODEL_ABDOMINAL_SEGMENTATION_HOST, MODEL_ABDOMINAL_SEGMENTATION_PORT
+
 from core.adapters.dicom_file import read_dicom_as_np_ndarray_and_normalise
+
 from core.adapters.nifti_file import (
     convert_dicom_np_ndarray_to_nifti_image,
     read_nifti_as_np_array,
     write_nifti_image,
 )
+from core.adapters.obj_file import write_mesh_as_obj
+from core.adapters.glb_file import convert_obj_to_glb_and_write
+
 from core.clients import niftynet
 from core.services.np_image_manipulation import downscale_and_conditionally_crop
-from core.tasks.abdominal_organs_segmentation.split_to_separate_organs import (
-    split_to_separate_organs,
-)
+from core.services.marching_cubes import generate_mesh
+# from core.tasks.abdominal_organs_segmentation.split_to_separate_organs import (
+#     split_to_separate_organs,
+# )
 from core.tasks.shared.receive_input import fetch_and_unzip
+from core.tasks.shared.dispatch_output import dispatch_output
+
 from jobs.jobs_io import (
     get_input_directory_path_for_job,
     get_logger_for_job,
@@ -32,8 +41,14 @@ from jobs.jobs_io import (
 )
 from jobs.jobs_state import JobState, update_job_state
 
+this_plid = os.path.basename(__file__).replace(".py", "")
+hu_threshold = 0
+
+
+
 
 def run(job_id: str, input_endpoint: str, medical_data: dict):
+
     logger = get_logger_for_job(job_id)
     update_job_state(job_id, JobState.STARTED.name, logger)
 
@@ -43,33 +58,38 @@ def run(job_id: str, input_endpoint: str, medical_data: dict):
 
     update_job_state(job_id, JobState.READING_INPUT.name, logger)
     dicom_image_array = read_dicom_as_np_ndarray_and_normalise(dicom_directory_path)
+    crop_dicom_image_array = downscale_and_conditionally_crop(dicom_image_array)
+
 
     update_job_state(job_id, JobState.PREPROCESSING.name, logger)
-    nifti_image = convert_dicom_np_ndarray_to_nifti_image(dicom_image_array)
+    nifti_image = convert_dicom_np_ndarray_to_nifti_image(crop_dicom_image_array)
     nifti_output_path = get_temp_file_path_for_job(job_id, "temp.nii")
     write_nifti_image(nifti_image, nifti_output_path)
 
     update_job_state(job_id, JobState.PERFORMING_SEGMENTATION.name, logger)
-    segmented_output_file_path = get_temp_file_path_for_job(job_id, "segmented.nii")
+    segmented_output_file_path = get_temp_file_path_for_job(job_id, "segmented.nii.gz")
     niftynet.call_model(
         MODEL_ABDOMINAL_SEGMENTATION_HOST,
-        MODEL_ABDOMINAL_SEGMENTATION_PORT,
+        int(MODEL_ABDOMINAL_SEGMENTATION_PORT),
         nifti_output_path,
         segmented_output_file_path,
     )
 
-    update_job_state(job_id, JobState.POSTPROCESSING.name, logger)
     segmented_array = read_nifti_as_np_array(
         segmented_output_file_path, normalise=False
     )
-    segmented_array = downscale_and_conditionally_crop(segmented_array)
-    split_to_separate_organs(segmented_array, get_result_file_path_for_job(job_id))
+    obj_output_path = get_temp_file_path_for_job(job_id, "temp.obj")
+    verts, faces, norm = generate_mesh(segmented_array, hu_threshold)
+    write_mesh_as_obj(verts, faces, norm, obj_output_path)
 
-    # TODO: This expects 1 output instead of 8, and as outputs/out.glb
-    # update_job_state(job_id, JobState.DISPATCHING_OUTPUT.name, logger)
-    # dispatch_output(job_id, this_plid, medical_data)
+    update_job_state(job_id, JobState.POSTPROCESSING.name, logger)
+    convert_obj_to_glb_and_write(obj_output_path, get_result_file_path_for_job(job_id))
 
-    # update_job_state(job_id, JobState.FINISHED.name, logger)
+    update_job_state(job_id, JobState.DISPATCHING_OUTPUT.name, logger)
+    result = dispatch_output(job_id, this_plid, medical_data)
+
+    update_job_state(job_id, JobState.FINISHED.name, logger)
+    return result
 
 
 if __name__ == "__main__":

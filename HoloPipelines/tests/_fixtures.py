f312257f8c558c7028c08da85ac4e39b33e31336
Niels Boecker <niels.boecker@gmail.com>
2019-08-24 16:51:30 +0100

    PIPELINE: Add jobs_io fixtures

---
+++ HoloPipelines/tests/_fixtures.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+from pytest import fixture
+
+from jobs import jobs_io
+
+
+@fixture
+def patch_jobs_io_and_create_dirs(monkeypatch: Any, job_id: str) -> None:
+    """
+    Patches jobs_io module, such that all its functions like get_log_file_path_for_job
+    will prefix their paths with "__test__jobs" instead
+    of "__jobs__".
+    Also creates the directories for this test's job_id
+    :param monkeypatch: injected by pytest
+    :param job_id: id for this test job (injected by pytest when test provides as param)
+    """
+    monkeypatch.setattr("jobs.jobs_io.jobs_root", "./__test_jobs__")
+    monkeypatch.setattr("jobs.jobs_io.finished_jobs_root", "./__test_finished_jobs__")
+
+    jobs_io.init_create_job_state_root_directories()
+    jobs_io.create_directory_for_job(job_id)
---
+++ HoloPipelines/tests/_input_utils.py
@@ -0,0 +1,19 @@
+sample_medical_data = {
+    "title": "Test Hologram",
+    "description": "Synthetic data for testing purposes",
+    "bodySite": "None",
+    "dateOfImaging": "2017-07-21T17:32:28Z",
+    "author": {
+        "aid": "123123",
+        "name": {
+            "full": "Kawai Wong",
+            "title": "Mr",
+            "given": "Kawai",
+            "family": "Wong",
+        },
+    },
+    "patient": {
+        "pid": "321321",
+        "name": {"full": "Jason Lee", "title": "Mr", "given": "Jason", "family": "Lee"},
+    },
+}

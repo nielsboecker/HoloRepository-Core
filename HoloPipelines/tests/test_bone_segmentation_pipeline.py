a3af7220d9359dffad2c1eac122c51649e98920e
Niels Boecker <niels.boecker@gmail.com>
2019-08-24 19:59:47 +0100

    PIPELINE: Add fixture to mock out HoloStorageAccessor

--- HoloPipelines/tests/test_bone_segmentation_pipeline.py
+++ HoloPipelines/tests/test_bone_segmentation_pipeline.py
@@ -1,10 +1,12 @@
 import os
+from typing import Any
+from unittest import mock
 
 import pytest
 
 from core.pipelines import bone_segmentation
 from ._input_utils import sample_medical_data
-from ._fixtures import patch_jobs_io_and_create_dirs  # noqa: F401
+from ._fixtures import patch_jobs_io_and_create_dirs, mock_send_to_holostorage_accessor
 
 this_test_name = os.path.basename(__file__).replace(".py", "")
 
@@ -14,5 +16,9 @@ imagingStudyEndpoint = (
 
 
 @pytest.mark.parametrize("job_id", [this_test_name])
-def test_pipeline(patch_jobs_io_and_create_dirs, job_id):  # noqa: F811
+def test_pipeline(
+    patch_jobs_io_and_create_dirs: Any,
+    mock_send_to_holostorage_accessor: mock.MagicMock,
+    job_id: str,
+):
     bone_segmentation.run(job_id, imagingStudyEndpoint, sample_medical_data)

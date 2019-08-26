abe08f5487215b96a35e4b3ebb66c83a9a66aad5
UdomkarnBoonyaprasert <udomkarnb@gmail.com>
2019-08-26 12:25:06 +0100

    PIPELINE: update .env

--- HoloPipelines/core/wrappers/simplify.py
+++ HoloPipelines/core/wrappers/simplify.py
@@ -3,7 +3,7 @@ import os
 import stat
 import subprocess
 
-from config import SIMPLIFICATION_RATIO  # TODO acutally add this in config
+from config import SIMPLIFICATION_RATIO
 
 STAT_OWNER_EXECUTABLE = stat.S_IEXEC
 
@@ -13,7 +13,7 @@ current_script_directory = os.path.dirname(os.path.realpath(__file__))
 def call_simplify(
     obj_input_path: str,
     obj_output_path: str,
-    simplification_ratio: float = float(SIMPLIFICATION_RATIO),
+    simplification_ratio: float = SIMPLIFICATION_RATIO,
 ):
     # NOTE: link to simplify repository https://github.com/sp4cerat/Fast-Quadric-Mesh-Simplification
     simplify_command = [

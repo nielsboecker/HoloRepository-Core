f5022b7587929944b0f0743f9d89816dd0fb5c68
UdomkarnBoonyaprasert <udomkarnb@gmail.com>
2019-08-26 10:45:09 +0100

    PIPELINE: add test cases for nifti normalise

--- HoloPipelines/tests/test_nifti_file.py
+++ HoloPipelines/tests/test_nifti_file.py
@@ -3,6 +3,7 @@ import sys
 import os
 import requests
 import nibabel
+import numpy
 import logging
 from zipfile import ZipFile
 
@@ -11,16 +12,17 @@ from core.adapters.nifti_file import (
     extract_np_array_from_nifti_image,
     extract_np_array_from_nifti_image_and_normalise,
     read_nifti_image,
+    read_nifti_as_np_array,
+    write_nifti_image,
+    convert_dicom_np_ndarray_to_nifti_image,
 )
 
-# read_nifti_as_np_array,
-# write_nifti_image,
-# convert_dicom_np_ndarray_to_nifti_image,
 
 test_input_path = "__test_input__"
 nifti_directory_path = f"{test_input_path}/nifti"
 sample_nifti_file = "1103_3_glm.nii"
 sample_nifti_zipped_file = "__test_nifti__.nii.zip"
+sample_output_nifti_file = "__test_nifti_output__.nii"
 
 
 @pytest.fixture
@@ -49,11 +51,21 @@ def test_setup():
 
     logging.info("setup: done")
 
+    # delete output file if exists
+    if os.path.exists(f"{nifti_directory_path}/{sample_output_nifti_file}"):
+        os.remove(f"{nifti_directory_path}/{sample_output_nifti_file}")
+
 
 # read nifti
 def test_read_nifti(test_setup):
     nifti_image = read_nifti_image(f"{nifti_directory_path}/{sample_nifti_file}")
     assert isinstance(nifti_image, nibabel.nifti1.Nifti1Image)
+
+
+# read nifti as numpy array
+def test_read_nifti_as_np_array(test_setup):
+    nifti_image = read_nifti_as_np_array(f"{nifti_directory_path}/{sample_nifti_file}")
+    assert isinstance(nifti_image, numpy.ndarray)
 
 
 # normalise nifti that is already normalised (so the shape has to be the same after rescaling)
@@ -65,3 +77,18 @@ def test_normalise_nifti(test_setup):
         nifti_image
     )
     assert normalised_nifti_image_as_np.shape == nifti_image_as_np.shape
+
+
+# writing nifti file
+def test_write_nifti(test_setup):
+    nifti_image = read_nifti_image(f"{nifti_directory_path}/{sample_nifti_file}")
+    write_nifti_image(nifti_image, f"{nifti_directory_path}/{sample_output_nifti_file}")
+    assert os.path.exists(f"{nifti_directory_path}/{sample_output_nifti_file}")
+
+
+# convert numpy array to nifti
+def test_convert_np_to_nifti(test_setup):
+    nifti_image = read_nifti_image(f"{nifti_directory_path}/{sample_nifti_file}")
+    nifti_image_as_np = extract_np_array_from_nifti_image(nifti_image)
+    nifti_image_from_np = convert_dicom_np_ndarray_to_nifti_image(nifti_image_as_np)
+    assert isinstance(nifti_image_from_np, nibabel.nifti1.Nifti1Image)

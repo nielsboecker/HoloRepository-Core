74ac6a86ebe5e1e6c49a113befbaa5671aa9ac76
Niels Boecker <niels.boecker@gmail.com>
2019-08-26 12:42:46 +0100

    UI/BE: Change redirect HTTP code to 307
    Code 308 was meant to represent a permanent redirect. While
    semantically accurate, this lead to some difficulties in
    development. Changed to 307, as it is non-cacheable.

--- HoloRepositoryUI/server/src/api/routes/holograms/holograms.controller.ts
+++ HoloRepositoryUI/server/src/api/routes/holograms/holograms.controller.ts
@@ -3,21 +3,21 @@ import { Request, Response } from "express";
 
 export class HologramsController {
   public async getAll(req: Request, res: Response): Promise<void> {
-    res.redirect(308, HologramsService.getAllForPatientsURL(req.query.pids));
+    res.redirect(307, HologramsService.getAllForPatientsURL(req.query.pids));
   }
 
   public downloadById(req: Request, res: Response): void {
-    res.redirect(308, HologramsService.getDownloadURL(req.params.hid));
+    res.redirect(307, HologramsService.getDownloadURL(req.params.hid));
   }
 
   public deleteById(req: Request, res: Response): void {
-    res.redirect(308, HologramsService.getEndpointURL(req.params.hid));
+    res.redirect(307, HologramsService.getEndpointURL(req.params.hid));
   }
 
   public upload(req: Request, res: Response): void {
     // Note: Ideally, the request should be checked for validity
     // Note: Upload models goes directly to the POST /holograms of HoloStorageAccessor
-    res.redirect(308, HologramsService.getBaseURL());
+    res.redirect(307, HologramsService.getBaseURL());
   }
 }
 
--- HoloRepositoryUI/server/src/api/routes/pipelines/pipelines.controller.ts
+++ HoloRepositoryUI/server/src/api/routes/pipelines/pipelines.controller.ts
@@ -3,17 +3,17 @@ import { Request, Response } from "express";
 
 export class PipelinesController {
   public getAll(req: Request, res: Response): void {
-    res.redirect(308, PipelinesService.getPipelinesURL());
+    res.redirect(307, PipelinesService.getPipelinesURL());
   }
 
   public getStatus(req: Request, res: Response): void {
-    res.redirect(308, PipelinesService.getJobStatusURL(req.params.jid));
+    res.redirect(307, PipelinesService.getJobStatusURL(req.params.jid));
   }
 
   public generate(req: Request, res: Response): void {
     // Note: Ideally, the request should be checked for validity
     // Note: Upload models goes directly to the POST /job of HoloPipelines
-    res.redirect(308, PipelinesService.getJobURL());
+    res.redirect(307, PipelinesService.getJobURL());
   }
 }
 

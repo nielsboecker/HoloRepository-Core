f6d759b0f4f1529d5e811732ca18406e84b1c759
Niels Boecker <niels.boecker@gmail.com>
2019-08-26 18:39:42 +0100

    PIPELINE: Refactor dicom_file_adapter test

--- HoloPipelines/tests/test_adapters_dicom.py
+++ HoloPipelines/tests/test_adapters_dicom.py
@@ -6,36 +6,31 @@ from core.adapters.dicom_file import (
     read_dicom_as_np_ndarray_and_normalise,
 )
 
-input_dir = "./tests/utils/sample_dicom"
-dicom_np_array = read_dicom_pixels_as_np_ndarray(input_dir)
+sample_dicom_directory_path = "./tests/utils/sample_dicom"
 
 
 def test_read_dicom_dataset():
-
-    assert len(read_dicom_dataset(input_dir)) == 4
+    result = read_dicom_dataset(sample_dicom_directory_path)
+    assert len(result) == 4
 
 
 def test_read_dicom_pixels_as_np_ndarray():
-
-    assert read_dicom_pixels_as_np_ndarray(input_dir).shape == (512, 512, 4)
-    # assert mock.called
-
-
-def test_flip_numpy_array_dimensions():
-
-    assert len(flip_numpy_array_dimensions())
+    result = read_dicom_pixels_as_np_ndarray(sample_dicom_directory_path)
+    assert result.shape == (512, 512, 4)
 
 
 def test_normalise_dicom():
-
-    assert normalise_dicom(dicom_np_array, input_dir).shape == (256, 302, 2)
+    dicom_as_np_array = read_dicom_pixels_as_np_ndarray(sample_dicom_directory_path)
+    result = normalise_dicom(dicom_as_np_array, sample_dicom_directory_path)
+    assert result.shape == (256, 302, 2)
 
 
 def test_flip_numpy_array_dimensions():
-
-    assert flip_numpy_array_dimensions(dicom_np_array).shape == (4, 512, 512)
+    dicom_as_np_array = read_dicom_pixels_as_np_ndarray(sample_dicom_directory_path)
+    result = flip_numpy_array_dimensions(dicom_as_np_array)
+    assert result.shape == (4, 512, 512)
 
 
 def test_read_dicom_as_np_ndarray_and_normalise():
-
-    assert read_dicom_as_np_ndarray_and_normalise(input_dir).shape == (256, 302, 2)
+    result = read_dicom_as_np_ndarray_and_normalise(sample_dicom_directory_path)
+    assert result.shape == (256, 302, 2)

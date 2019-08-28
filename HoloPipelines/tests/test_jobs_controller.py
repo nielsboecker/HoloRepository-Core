d86d10f0a9af54208351cf74a31b52ef56b6a916
Niels Boecker <niels.boecker@gmail.com>
2019-08-28 19:28:53 +0100

    PIPELINE: Refactor test_jobs_controller

--- HoloPipelines/tests/test_jobs_controller.py
+++ HoloPipelines/tests/test_jobs_controller.py
@@ -1,27 +1,27 @@
 from unittest import mock
 
-import os
 from jobs.jobs_controller import (
-    start_new_job,
     check_job_request_validity,
     create_random_job_id,
-    init_job,
+    start_new_job,
 )
 from tests.utils.input_data import sample_job
 
 
-this_test_name = os.path.basename(__file__).replace(".py", "")
-
-
 def test_start_new_job():
-    assert start_new_job(sample_job)
+    result_success, result_job_response = start_new_job(sample_job)
+    assert result_success is True
+    assert isinstance(result_job_response, dict)
+    assert isinstance(result_job_response["jid"], str)
 
 
 def test_check_job_request_validity():
     with mock.patch("jobs.jobs_controller.init_job") as mock_init_job:
         mock_init_job.return_value = create_random_job_id()
-    assert check_job_request_validity(sample_job)
+    result, _ = check_job_request_validity(sample_job)
+    assert result is True
 
 
 def test_create_random_job_id():
-    assert len(create_random_job_id()) == 16
+    result = create_random_job_id()
+    assert len(result) == 16

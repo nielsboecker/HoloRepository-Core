a64d1fae9fc9405ce4eccd39685c6b7556d6aec1
carlo3247 <cawinkelhake@aol.com>
2020-04-01 18:11:55 +0100

    updated pipeline template file

--- HoloPipelines/core/pipelines/template.py
+++ HoloPipelines/core/pipelines/template.py
@@ -23,29 +23,24 @@ from jobs.jobs_state import JobState, update_job_state
 this_plid = os.path.basename(__file__).replace(".py", "")
 
 
-def run(job_id: str, input_endpoint: str, medical_data: dict) -> None:
+def run(job_id: str, pipeline_metadata: dict, input_endpoint: str, medical_data: dict) -> None:
     logger = get_logger_for_job(job_id)
     update_job_state(job_id, JobState.STARTED.name, logger)
-
 
     update_job_state(job_id, JobState.FETCHING_INPUT.name, logger)
     dicom_directory_path = get_input_directory_path_for_job(job_id)
     fetch_and_unzip(input_endpoint, dicom_directory_path)
-
 
     update_job_state(job_id, JobState.READING_INPUT.name, logger)
     dicom_image: np.ndarray = read_dicom_as_np_ndarray_and_normalise(
         dicom_directory_path
     )
 
-
     update_job_state(job_id, JobState.PREPROCESSING.name, logger)
     downscaled_image = downscale_and_conditionally_crop(dicom_image)
 
-
     update_job_state(job_id, JobState.PERFORMING_SEGMENTATION.name, logger)
     # Insert segmentation here
-
 
     update_job_state(job_id, JobState.POSTPROCESSING.name, logger)
     verts, faces, norm = generate_mesh(downscaled_image, 0)
@@ -53,10 +48,8 @@ def run(job_id: str, input_endpoint: str, medical_data: dict) -> None:
     write_mesh_as_obj(verts, faces, norm, obj_output_path)
     convert_obj_to_glb_and_write(obj_output_path, get_result_file_path_for_job(job_id))
 
-
     update_job_state(job_id, JobState.DISPATCHING_OUTPUT.name, logger)
     dispatch_output(job_id, this_plid, medical_data)
-
 
     update_job_state(job_id, JobState.FINISHED.name, logger)
 
